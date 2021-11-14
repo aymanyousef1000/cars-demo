98b1db62d939f682e813abc3e8c82c74cfd3130a
aymanyousef1000 <aymanyousef1000@gmail.com>
2021-11-14 13:23:04 +0200

    edit model name

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models;
-use App\Models\Car;
+use App\Models\car;
 use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
     //
     public function index(){
-        $data = Car::get();
+        $data = car::get();
         return response()->json($data,200);
-        
+
     }
 }
